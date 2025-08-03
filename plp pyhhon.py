9f7af83484085640bec4f7b34cf6569fe590cc12
misgana3 <misganaw357@gmail.com>
2025-08-03 10:20:18 +0300

    Update plp pyhhon.py

--- plp pyhhon.py
+++ plp pyhhon.py
@@ -1,15 +1,9 @@
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> # Simple calculator program
-... 
-... # Ask the user to input two numbers
+
+>>> #Simple_calculator_progr
 ... num1 = float(input("Enter the first number: "))
 ... num2 = float(input("Enter the second number: "))
-... 
-... # Ask the user to choose an operation
 ... operation = input("Choose an operation (+, -, *, /): ")
-... 
-... # Perform the selected operation
+
 ... if operation == '+':
 ...     result = num1 + num2
 ...     print(f"The result of {num1} + {num2} is {result}")
@@ -27,3 +21,4 @@ Enter "help" below or click "Help" above for more information.
 ...         print("Error: Division by zero is not allowed.")
 ... else:
 ...     print("Invalid operation. Please choose +, -, *, or /.")
+
